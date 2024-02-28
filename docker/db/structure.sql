61d9a8b48b52aee05de537b09ea59f8e74c6f553
Sia <assiya.serb@gmail.com>
2024-02-28 22:25:33 +0200

    Lab2

---
+++ docker/db/structure.sql
@@ -0,0 +1,11 @@
+CREATE TABLE processed_agent_data (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    road_state VARCHAR(255) NOT NULL,
+    x FLOAT,
+    y FLOAT,
+    z FLOAT,
+    latitude FLOAT,
+    longitude FLOAT,
+    timestamp TIMESTAMP
+);
